9a26296a7e10dc0c79cdac0f156d137883fa9049
wanderingpuppet <110518339+wanderingpuppet@users.noreply.github.com>
2022-08-13 14:38:26 +0700

    Solve repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num < 0) return 'ERROR';
 
+  let repeated = '';
+  for (let i = 0; i < num; i++) {
+    repeated += string;
+  }
+  return repeated;
 };
 
 // Do not edit below this line
