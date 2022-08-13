16a9422ba737b46973719de900f1b23c517286cd
wanderingpuppet <110518339+wanderingpuppet@users.noreply.github.com>
2022-08-13 15:21:29 +0700

    Solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  // Both arguments must be non-negative numbers
+  if (typeof a != 'number' || typeof b != 'number' || a < 0 || b < 0) {
+    return 'ERROR';
+  }
 
+  // Make sure that a will always be the smaller number
+  if (a > b) [a, b] = [b, a];
+
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
