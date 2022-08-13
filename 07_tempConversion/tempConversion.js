032e2b6ed705bb60050980417a6b6b6780e9c973
wanderingpuppet <110518339+wanderingpuppet@users.noreply.github.com>
2022-08-13 15:57:12 +0700

    Solve tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celsius = (fahrenheit - 32)/1.8;
+  return +celsius.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let fahrenheit = celsius*1.8 + 32;
+  return +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
