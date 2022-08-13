06156a787de392b1bcb662ae31bd324c4e60309e
wanderingpuppet <110518339+wanderingpuppet@users.noreply.github.com>
2022-08-13 14:59:12 +0700

    Solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...elements) {
+  let i = 0;
+  while (i < arr.length) {
+    if (elements.includes(arr[i])) {
+      arr.splice(i, 1);
+      i--;
+    }
+    i++;
+  }
+  return arr;
 };
 
 // Do not edit below this line
