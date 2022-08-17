d1f5cb111040e2ffe4957b84fee9a4b3295a5acf
wanderingpuppet <110518339+wanderingpuppet@users.noreply.github.com>
2022-08-17 10:52:41 +0700

    Solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  // Sort people from oldest to youngest
+  people.sort((a, b) => {
+    let currentYear = (new Date()).getFullYear();
 
+    // Calculate a person age using the current year if they are still alive
+    let aYearOfDeath = a.yearOfDeath !== undefined ? a.yearOfDeath : currentYear;
+    let bYearOfDeath = b.yearOfDeath !== undefined ? b.yearOfDeath : currentYear;
+
+    let aAge = aYearOfDeath - a.yearOfBirth;
+    let bAge = bYearOfDeath - b.yearOfBirth;
+    return bAge - aAge;
+  });
+  return people[0];
 };
 
 // Do not edit below this line
