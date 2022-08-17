84f5edb6496bcc4df5a7eac97092484ea48c0a44
wanderingpuppet <110518339+wanderingpuppet@users.noreply.github.com>
2022-08-17 10:38:28 +0700

    Solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) return "OOPS";
 
+  let a = 0;
+  let b = 1;
+
+  for (let i = 1; i < n; i++) {
+    [a, b] = [b, a + b];
+  }
+  return b;
 };
 
 // Do not edit below this line
