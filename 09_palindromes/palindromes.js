ea57fb5ac0263336822b0415c94bac3357cde14d
wanderingpuppet <110518339+wanderingpuppet@users.noreply.github.com>
2022-08-17 10:31:43 +0700

    Solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.toLowerCase();
+  let left = 0;
+  let right = string.length - 1;
 
+  while (left < right) {
+    let leftChar = string[left];
+    let rightChar = string[right];
+
+    // Skip punctuations
+    if (leftChar < 'a' || leftChar > 'z') {
+      left++;
+      continue;
+    }
+    if (rightChar < 'a' || rightChar > 'z') {
+      right--;
+      continue;
+    }
+
+    if (leftChar != rightChar) {
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+  return true;
 };
 
 // Do not edit below this line
