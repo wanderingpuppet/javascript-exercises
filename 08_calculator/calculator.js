e709fc18c791ef984cbbc6663834cca934804713
wanderingpuppet <110518339+wanderingpuppet@users.noreply.github.com>
2022-08-17 10:00:18 +0700

    Solve calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, n) => total + n, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, n) => product * n, 1);
 };
 
-const power = function() {
-	
+const power = function(n, p) {
+  return n ** p;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let product = 1;
+  for (let i = 1; i <= n; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
